f125cc82cec3747d9955a89426ebbe43da4d34ee
Ngoc Thao <trannguyenngocthao8327@gmail.com>
2020-05-22 06:25:43 +0700

    master

--- 7-excercise work/arrFindAndFilter..js
+++ 7-excercise work/arrFindAndFilter..js
@@ -9,11 +9,27 @@
 
 
 
-// Given an array of numbers, return a new array that only includes the even numbers.
-function evensOnly(arr) {
-    var result = arr.filter(function(item){
-        return item % 2 === 0;
-    });
-    return result;
-  }
-console.log(evensOnly(1, 3, 5, 8));  //TypeError: arr.filter is not a function      
+// // Given an array of numbers, return a new array that only includes the even numbers.
+// function evensOnly(arr) {
+//     var result = arr.filter(function(item){
+//         return item % 2 === 0;
+//     });
+//     return result;
+//   }
+// console.log(evensOnly(1, 3, 5, 8));  //TypeError: arr.filter is not a function      
+
+
+
+// Viết hàm trả về số chẵn đầu tiên trong mảng sử dụng find method
+// Nếu không tìm thấy số chẵn nào thì trả về undefined
+function findFirstEvenNumber(array) {
+  var soChan = array.find(function(item) {
+    return item % 2 === 0;
+  });
+  if (soChan === 0) return 'underfind';
+  if (soChan !== 0) return soChan;
+  
+};
+console.log(findFirstEvenNumber([1, 2, 3, 4]));
+  
+
